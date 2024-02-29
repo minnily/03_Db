ddd9e4111c3b2247a0cfd925891d5a85d2bd8295
JUHAHAH <156267800+minnily@users.noreply.github.com>
2024-02-29 17:38:14 +0900

    2.29 학원

---
+++ 00_사용자_계정_생성.sql
@@ -0,0 +1,22 @@
+
+-- 한줄 주석 
+
+/*
+ * 범위 주석
+ * 
+ * */
+
+-- 11G 버전 이전의 문법을 사용 가능하도록 함
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+-- CTRL + ENTER : 선택한 SQL 수행
+
+-- 사용자 계정 생성
+CREATE USER kh_lmk IDENTIFIED BY kh1234;
+
+
+-- 사용자 계정에 권한 부여
+GRANT RESOURCE, CONNECT TO kh_lmk;
+
+-- 객체가 생성될 수 있는 공간 할당량 지정
+ALTER USER kh_lmk DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
